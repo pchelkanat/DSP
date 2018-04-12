68fc4fa2b83349d4c7af8025ba8486ab0e85c34b
pchelkanat <pchelkanat@mail.ru>
2018-04-12 21:49:16 +0300

    Преобразован в функции. D, B - строка, столбец. N=10 локально

--- Homeworks/watermarking_lsb.py
+++ Homeworks/watermarking_lsb.py
@@ -8,63 +8,88 @@ Len = np.shape(origin)
 print(origin, Len)
 
 s = "Natalya's watermark"
-# print(len(s))
-sbytes = s.encode("utf-8")
-sbits = bs.BitArray(sbytes).bin
-N = len(sbits)
-
-# def Origin2Orbit(origin):
-originbytes = np.zeros_like(origin, dtype=list)
-for i in range(Len[0]):
-    originbytes[i] = list(format(origin[i], 'b').replace("-", "").zfill(16))
-# print(originbytes, type(originbytes))
-# return originbytes
 
 
+def s2bit(s):
+    # print(len(s))
+    sbytes = s.encode("utf-8")
+    sbits = bs.BitArray(sbytes).bin
+    N = len(sbits)
+    return sbits, N
 
-A = np.eye(10 - 1, 10, k=1, dtype=np.int64)  # опр. матрица
-q = (-1 * np.random.randint(0, 2, (1, 10), dtype=np.int64)) % 2  # многочлен
-q[0, 0] = 1
-A = np.vstack((A, q))
-# print(A)
 
-D = np.zeros((10,1), dtype=np.int64)  # вектор-столбец Nx1
-D[0][0] = 1
-B = D.T  # вектор-строка 1xN
-print(D, np.shape(D))
-print(B, np.shape(B))
+def Origin2Orbit(origin):
+    originbytes = np.zeros_like(origin, dtype=list)
+    for i in range(Len[0]):
+        originbytes[i] = list(format(origin[i], 'b').replace("-", "").zfill(16))
+    # print(originbytes, type(originbytes))
+    return originbytes
 
-S = np.ones((11, 10), dtype=np.int64)
-S[0] = np.random.randint(0, 2, (1, 10), dtype=np.int64)  # ненулевой вектор-строка, 1xN
-# в дальнейшем чтобы получить столбец будем транспонировать
-if (S[0].sum() == 0):
-    S[0, 0] = 1
-print()
 
-y = np.zeros(10,dtype=np.int64)
-for k in range(10):
-    # print(np.shape(A), np.shape(S[k]), np.shape(B))
-    # print("ss", S[k],np.shape(S[k]))
-    print(k, sbits[k])
-    temp1 = (np.dot(A, S[k].T) + (B * int(sbits[k]))) % 2  # NxN * Nx1 = Nx1, Nx1 * число = Nx1
-    """
-    t1 = np.dot(A, S[k].T)
-    t2 = B * int(sbits[k])
-    print('t1', t1, np.shape(t1))
-    print('t2', t2, np.shape(t2))
-    """
+def ADBS():
+    A = np.eye(10 - 1, 10, k=1, dtype=np.int64)  # опр. матрица
+    q = (-1 * np.random.randint(0, 2, (1, 10), dtype=np.int64)) % 2  # многочлен
+    q[0, 0] = 1
+    A = np.vstack((A, q))
+    # print(A)
 
-    print(temp1, np.shape(temp1))
-    S[k + 1] = temp1  # сохраняем опять в строку, но для y[k] он столбец
-    y[k] = np.dot(D.T, temp1.T)  # 1xN * Nx1 = число
-    print('y',y[k])
+    D = np.zeros(10, dtype=np.int64)  # вектор-столбец Nx1
+    D[0] = 1
+    B = D.T  # вектор-строка 1xN
+    print(D, np.shape(D))
+    print(B, np.shape(B))
 
-print()
-print("s", S)
-print("YY",y)
+    S = np.ones((11, 10), dtype=np.int64)
+    S[0] = np.random.randint(0, 2, (1, 10), dtype=np.int64)  # ненулевой вектор-строка, 1xN
+    # в дальнейшем чтобы получить столбец будем транспонировать
+    if (S[0].sum() == 0):
+        S[0, 0] = 1
+    return A, D, B, S
 
+
+def convertWtr(A, D, B, S, sbits):
+    y = np.zeros(10, dtype=np.int64)
+    for k in range(10):
+        # print(np.shape(A), np.shape(S[k]), np.shape(B))
+        # print("ss", S[k],np.shape(S[k]))
+        # print(k, sbits[k])
+        temp1 = (np.dot(A, S[k]) + (B * int(sbits[k]))) % 2  # NxN * Nx1 = Nx1, Nx1 * число = Nx1
+        """
+        t1 = np.dot(A, S[k].T)
+        t2 = B * int(sbits[k])
+        print('t1', t1, np.shape(t1))
+        print('t2', t2, np.shape(t2))
+        """
+
+        # print(temp1, np.shape(temp1))
+        S[k + 1] = temp1  # сохраняем опять в строку, но для y[k] он столбец
+        y[k] = np.dot(D, temp1) % 2  # 1xN * Nx1 = число
+        # print('y',y[k])
+    return y
+
+
+def restoreWtr(A, D, S, y):
+    x = np.zeros(10, dtype=np.int64)
+    for k in range(10):
+        x[k] = (y[k] + np.linalg.multi_dot([D, A, S[k]])) % 2
+    return x
+
+
+def __init__():
+    A, D, B, S = ADBS()
+    sbits, N = s2bit(s)
+
+    y = convertWtr(A, D, B, S, sbits)
+    x = restoreWtr(A, D, S, y)
+    print("y",y)
+    print("x",x)
+
+__init__()
+
+"""
 sbytes_d = int(sbits, 2).to_bytes(len(sbytes), 'big')  # начало с MSB
 msg = sbytes_d.decode('utf-8')
+"""
 """
 """
 # print(list(sbits), N, type(sbits), N)
