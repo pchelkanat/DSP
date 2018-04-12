1437e434199ee6fb2ea4c7dd9f1ee41dcef3ffa5
pchelkanat <pchelkanat@mail.ru>
2018-04-13 00:11:22 +0300

    Заминка в адресации LPM2

--- Homeworks/watermarking_lsb.py
+++ Homeworks/watermarking_lsb.py
@@ -2,13 +2,6 @@ import bitstring as bs
 import numpy as np
 import scipy.io.wavfile as sw
 
-fs, origin = sw.read("voice.wav")
-origin = np.int64(origin)
-Len = np.shape(origin)
-print(origin, Len)
-
-s = "Natalya's watermark"
-
 
 def s2bit(s):
     # print(len(s))
@@ -18,39 +11,44 @@ def s2bit(s):
     return sbits, N
 
 
-def Origin2Orbit(origin):
+def origin2orbit(origin, Len):
     originbytes = np.zeros_like(origin, dtype=list)
+
     for i in range(Len[0]):
-        originbytes[i] = list(format(origin[i], 'b').replace("-", "").zfill(16))
-    # print(originbytes, type(originbytes))
+        originbytes[i] = (format(origin[i], 'b').replace("-", "").zfill(8))
+    # print(originbytes[1],originbytes[1][len(originbytes[0])-1])
+    # print(originbytes)
     return originbytes
 
 
 def ADBS(N):
-    A = np.eye(N - 1, N, k=1, dtype=np.int64)  # опр. матрица
-    q = (-1 * np.random.randint(0, 2, (1, N), dtype=np.int64)) % 2  # многочлен
+    A = np.eye(N - 1, N, k=1, dtype=np.int32)  # опр. матрица
+    q = (-1 * np.random.randint(0, 2, (1, N), dtype=np.int32)) % 2  # многочлен
     q[0, 0] = 1
+    # print("q", q)
     A = np.vstack((A, q))
     # print(A)
 
-    D = np.zeros(N, dtype=np.int64)  # вектор-столбец Nx1
+    # D = np.zeros(N, dtype=np.int64)  # вектор-столбец Nx1
+    D = np.random.randint(0, 2, N, dtype=np.int32)
     D[0] = 1
 
     B = D.T  # вектор-строка 1xN
-    #print(D, np.shape(D))
-    #print(B, np.shape(B))
+    # print(D, np.shape(D))
+    # print(B, np.shape(B))
 
-    S = np.ones((N+1, N), dtype=np.int64)
-    S[0] = np.random.randint(0, 2, (1, N), dtype=np.int64)  # ненулевой вектор-строка, 1xN
+    S = np.ones((N + 1, N), dtype=np.int32)
+    S[0] = np.random.randint(0, 2, (1, N), dtype=np.int32)  # ненулевой вектор-строка, 1xN
     # в дальнейшем чтобы получить столбец будем транспонировать
     if (S[0].sum() == 0):
         S[0, 0] = 1
+    # print("S[0]", S[0], np.shape(S))
     return A, D, B, S
 
 
 def convertWtr(A, D, B, S, sbits):
-    N=len(sbits)
-    y = np.zeros(N, dtype=np.int64)
+    N = len(sbits)
+    y = np.zeros(N, dtype=np.int32)
     for k in range(N):
         # print(np.shape(A), np.shape(S[k]), np.shape(B))
         # print("ss", S[k],np.shape(S[k]))
@@ -62,61 +60,77 @@ def convertWtr(A, D, B, S, sbits):
         print('t1', t1, np.shape(t1))
         print('t2', t2, np.shape(t2))
         """
-
-        # print(temp1, np.shape(temp1))
+        # print("temp", temp1, np.shape(temp1))
         S[k + 1] = temp1  # сохраняем опять в строку, но для y[k] он столбец
         y[k] = np.dot(D, temp1) % 2  # 1xN * Nx1 = число
         # print('y',y[k])
-    return y
+    return y, S
 
 
 def restoreWtr(A, D, S, y):
-    N=len(y)
-    x = np.zeros(N, dtype=np.int64)
+    N = len(y)
+    x = np.zeros(N, dtype=np.int32)
     for k in range(N):
         x[k] = (y[k] + np.linalg.multi_dot([D, A, S[k]])) % 2
     return x
 
 
-def __init__():
-
+def LPM1(s):
     sbits, N = s2bit(s)
     A, D, B, S = ADBS(N)
-
-    y = convertWtr(A, D, B, S, sbits)
+    y, S = convertWtr(A, D, B, S, sbits)
     x = restoreWtr(A, D, S, y)
-    print("y",y)
-    print("x",x)
-    print(sbits)
+
+    # print("A", A)
+    # print("D", D)
+    # print("B", B)
+    # print("S", S)
+
+    # print("y", y)
+    # print("x", x)
+    # print(sbits)
+    return y, x
+
+
+def LPM2(origin, Len, y, x):
+    A1, D1, B1, S1 = ADBS(len(y))
+    yy, S1 = convertWtr(A1, D1, B1, S1, x)
+    # print("A", A1)
+    # print("D", D1)
+    # print("B", B1)
+    # print("S", S1)
+
+    originbytes = origin2orbit(origin, Len)
+    print(np.size(origin))
+    for k in range(np.shape(S1)[0]):
+        yS = np.array2string(S1[k]).replace(" ", "").replace("[", "").replace("]", "").replace("\n", "")
+        yS=int(yS,2)
+        print(yS)
+        #ТАКОГО ПРОСТО НЕ СУЩЕСТВУЕТ
+        originbytes[yS] = int(originbytes[yS])
+        orBitLen = len(originbytes[yS])
+        print(originbytes[yS][orBitLen - 1],type(originbytes[yS][orBitLen - 1]))
+        originbytes[yS][orBitLen - 1] = str(y[k])
+    return originbytes
+
+
+def __init__():
+    fs, origin = sw.read("voice.wav")
+    origin = np.int32(origin)
+    shapes = np.shape(origin)
+
+    s = "Natalya's watermark"
+    ym, xm = LPM1(s)
+
+    LPM2(origin, shapes, ym, xm)
+
+
+#    orBytes=getAddress(origin, y1)
+#    return orBytes
 
 __init__()
 
 """
 sbytes_d = int(sbits, 2).to_bytes(len(sbytes), 'big')  # начало с MSB
 msg = sbytes_d.decode('utf-8')
-"""
-"""
-"""
-# print(list(sbits), N, type(sbits), N)
-# print(152 / 19)
-
-
-
-"""
-v = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-print(v)
-m = np.copy(v)
-c = np.array([[2, 3, 4]])
-d = c.T
-m[0] = [9, 8, 7]
-print(m - v)
-
-print(v, np.shape(v))
-print(c, np.shape(c))
-print(d, np.shape(d))
-print(c[0])
-print(d[0])
-print(v[0,1])
-print(np.dot(v,d))
-print(np.dot(v,d)+d*2)
 """
